0b45e3778e40bda563375519a8d949494b5bfaac
Connor Hoy <connorhoy116@gmail.com>
2017-03-16 18:25:08 -0400

    Updating repository.

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server 
 
-// Sets up the Express app to handle data parsing
+var app = express(); // Tells node that we are creating an "express" server
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-//Points server to needed files
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
-//Starts server
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or 
+// request data from various URLs. 
+// ================================================================================
+
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlroutes.js')(app);
+
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server 
+// ==============================================================================
+
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
